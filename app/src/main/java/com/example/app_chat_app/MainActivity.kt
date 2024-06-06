46fb5925bf039897e002b97f853d10e4993fd1ce
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-06-06 20:04:00 +0200

    updated code

--- app/src/main/java/com/example/app_chat_app/MainActivity.kt
+++ app/src/main/java/com/example/app_chat_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,16 @@ class MainActivity : AppCompatActivity() {
 
 
         val btnTest : Button = findViewById(R.id.btn_Test)
+
+
+        val switch = findViewById<SwitchCompat>(R.id.switch1)
+
+        switch. setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if()
+
+        }
+
 
 
         // BTN Listener -> Go 2 Sign Up Page
